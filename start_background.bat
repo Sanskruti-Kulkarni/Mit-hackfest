ba7f96159d337192e6d2996c237f42479a24597b
Sanskruti-Kulkarni <kulkarnisanskruti31@gmail.com>
2026-09-12 01:59:34 +0530

    Add files via upload

---
+++ start_background.bat
@@ -0,0 +1,10 @@
+@echo off
+title JalMitra Water Governance Server
+echo Starting JalMitra Background Service on http://localhost:3000 ...
+cd /d "%~dp0"
+if exist "C:\Users\Sanskruti\AppData\Roaming\Antigravity\bin\agy-node.cmd" (
+    start /B "" "C:\Users\Sanskruti\AppData\Roaming\Antigravity\bin\agy-node.cmd" server.js
+) else (
+    start /B "" node server.js
+)
+echo JalMitra is running in background at http://localhost:3000
